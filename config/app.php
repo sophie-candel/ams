aa70f3aa342b674c994f0809a1b28212af6c6334
Sophie Candel <sophie.candel@etu.unistra.fr>
2018-06-14 14:53:34 +0200

    Laratrust installation and setup

--- config/app.php
+++ config/app.php
@@ -160,6 +160,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+
+        Laratrust\LaratrustServiceProvider::class,
+
     ],
 
     /*
@@ -208,6 +211,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Laratrust'   => Laratrust\LaratrustFacade::class,
 
     ],
 
